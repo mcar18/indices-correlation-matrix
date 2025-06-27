a4ba8e7724aca8dfe804ea938c8f3904d5a3dd0e
mcar18 <84042230+mcar18@users.noreply.github.com>
2025-06-26 23:34:02 -0400

    test

--- market indices correlation matrix.py
+++ market indices correlation matrix.py
@@ -1,2 +1,8 @@
-import yfinance as yfinance
-#test
+import yfinance as yf
+import pandas as pd
+
+sectors = ['XLK','XLF','XLE','XLI','XLP','XLU','XLV','XLY','XLB','XLRE','XLK']
+data = yf.download(sectors, period='1y', interval='1d')['Adj Close'].pct_change().dropna()
+
+correlation_matrix = data.corr()
+#print(correlation_matrix)
